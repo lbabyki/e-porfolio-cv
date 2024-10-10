e38a5227f58f4cc9e3c378a7edf3b68aeaf1ae03
Giang <crglh25@gmail.com>
2024-10-10 15:02:24 +0700

    update footer

--- my-por/src/components/Footer.js
+++ my-por/src/components/Footer.js
@@ -1,10 +1,21 @@
 import React from "react";
+import logo from "../assets/img/Giang.png";
+import "../assets/style/Footer.css";
+import SocialIcons from "./SocialIcons";
 const Footer = () => {
   return (
     <footer>
-      <p style={{ color: "#7f8089" }}>
-        &copy; {new Date().getFullYear()} My Portfolio
-      </p>
+      <div className="footer-css">
+        <img src={logo} alt="My logo" />
+        CainG
+      </div>
+      <SocialIcons />
+      <div className="footer-copyright">
+        {" "}
+        <p style={{ color: "#7f8089" }}>
+          &copy; {new Date().getFullYear()} My Portfolio build by React
+        </p>
+      </div>
     </footer>
   );
 };
